e1ac9edd0f6ba0e22dfa569eab104cb566b143fc
okeno1 <petercoin.1997@gmail.com>
2022-07-05 15:57:47 -0700

    a function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * _isalpha - a function that checks for alphabetic character
+ *Description: _isalpha - a function that checks for alphabetic character
  * @c: single letter input
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
