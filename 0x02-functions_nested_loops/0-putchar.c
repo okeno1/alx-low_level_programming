610f2fc6e3fd1de876a69fccc61df19bef9f53b8
okeno1 <petercoin.1997@gmail.com>
2022-07-05 10:58:58 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	char *sh = "Okeno";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
