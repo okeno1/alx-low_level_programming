5ab0f86c35c54d7903965ba561a9a4f6a78fe98e
okeno1 <petercoin.1997@gmail.com>
2022-07-07 10:39:44 -0700

    Removing an infinite loop task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,10 @@ int main(void)
 {
 int i;
 
-     printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-        i = 0;
+i = 0;
+printf("Infinite loop avoided! \\o/\n");
 
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+return (0);
 }
