be0e56610994b63e45bb97330599df7122344430
okeno1 <petercoin.1997@gmail.com>
2022-07-14 09:43:06 -0700

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *ptr = s;
 
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+	while (n-- > 0)
+		*ptr++ = b;
 
 	return (s);
 }
