12c18ab4cb710aa51a70d4b2f7c30e405beeecd6
okeno1 <petercoin.1997@gmail.com>
2022-08-01 12:43:54 -0700

    a new type struct dog with particular elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ *struct dog - structure
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
