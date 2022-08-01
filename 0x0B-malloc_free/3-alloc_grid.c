9af4c530ce359b8c47235e13a069fb3ef1b38d8d
okeno1 <petercoin.1997@gmail.com>
2022-08-01 02:47:09 -0700

    a function that returns a pointer to a 2 dimensional array of integers using double pointers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arrLoc[i] = (int *) malloc(sizeof(int *) * width);
+		arrLoc[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			free(arrLoc);
