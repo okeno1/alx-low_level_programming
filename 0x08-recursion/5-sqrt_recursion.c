aee6f7455a5c4476f919cfc37558969c71cf581c
okeno1 <petercoin.1997@gmail.com>
2022-08-02 10:37:10 -0700

    a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,17 +24,3 @@ int sqtChecker(int min, int max, int m)
 	}
 	return (-1);
 }
-
-/**
- *_sqrt_recursion - finds the natural square root of a number
- *@n: given number
- *Return: square root of n or -1
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	return (sqtChecker(1, n, n));
-}
