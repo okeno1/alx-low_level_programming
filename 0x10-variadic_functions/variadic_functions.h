b2dd4e3ea474940629f0da35a461cb3d95b3627d
okeno1 <petercoin.1997@gmail.com>
2022-08-04 09:35:15 -0700

    variadic_functions headers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef variadic_functions_h_
-#define variadic_functions_h_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
